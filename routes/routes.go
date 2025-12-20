88666b3ebd67a1b38cd6d850a808717479d9f7c4
bereket-7 <bekhonelign@gmail.com>
2025-12-21 01:28:33 +0300

    routes change

--- routes/routes.go
+++ routes/routes.go
@@ -11,7 +11,7 @@ func RegisterRoutes(r *gin.Engine, cfg *config.Config) {
     s := service.NewYayaService(cfg)
     v1 := r.Group("/api/v1")
     {
-        v1.POST("/payment", s.CreatePayment)
+        // v1.POST("/payment", s.CreatePayment)
         v1.POST("/webhook/yaya", s.HandleWebhook)
         v1.POST("/payment-intent", s.CreatePaymentIntent)
     }
